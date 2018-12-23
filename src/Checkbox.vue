645fd3ce630ccb29bf51697f84607c7dc238784e
weibo <919006380@qq.com>
2018-12-23 14:25:31 +0800

    添加并初始化checkbox组件和card组件.

---
+++ src/Checkbox.vue
@@ -0,0 +1,166 @@
+<template>
+  <div>
+    <div id="container">
+      <!-- <div id="container" @click="${() => this._toggleCheck()}"> -->
+      <div id="checkPanel" class="inline">
+        <svg id="svg" width="0" height="0"></svg>
+      </div>
+      <div class="inline">{{text}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { wired } from "./wired-lib.js";
+export default {
+    props:{
+        text:{
+            type:String
+        },
+        disabled:{
+            type:Boolean,
+            default:false
+        },
+        checked:{
+            type:Boolean,
+            default:false
+        }
+    },
+  mounted() {
+      this.$el.classList.add('pending');
+    // updated
+    const svg = this.$el.querySelector("#svg");
+    this._clearNode(svg);
+    const s = { width: 24, height: 24 };
+    svg.setAttribute("width", s.width);
+    svg.setAttribute("height", s.height);
+    wired.rectangle(svg, 0, 0, s.width, s.height);
+    const checkpaths = [];
+    checkpaths.push(
+      wired.line(
+        svg,
+        s.width * 0.3,
+        s.height * 0.4,
+        s.width * 0.5,
+        s.height * 0.7
+      )
+    );
+    checkpaths.push(
+      wired.line(svg, s.width * 0.5, s.height * 0.7, s.width + 5, -5)
+    );
+    checkpaths.forEach(d => {
+      d.style.strokeWidth = 2.5;
+    });
+    if (this.checked) {
+      checkpaths.forEach(d => {
+        d.style.display = "";
+      });
+    } else {
+      checkpaths.forEach(d => {
+        d.style.display = "none";
+      });
+    }
+    this.$el.classList.remove("pending");
+
+    this._setAria();
+    this._attachEvents();
+  },
+  methods: {
+    _onDisableChange() {
+      if (this.disabled) {
+        this.$el.classList.add("disabled");
+      } else {
+        this.$el.classList.remove("disabled");
+      }
+      this._refreshTabIndex();
+    },
+
+    _refreshTabIndex() {
+      this.tabIndex = this.disabled ? -1 : this.getAttribute("tabindex") || 0;
+    },
+
+    _setAria() {
+      this.$el.setAttribute("role", "checkbox");
+      this.$el.setAttribute("aria-checked", this.checked);
+      this.$el.setAttribute("aria-label", this.text);
+    },
+
+    _toggleCheck() {
+      this.checked = !(this.checked || false);
+      const event = new CustomEvent("change", {
+        bubbles: true,
+        composed: true,
+        checked: this.checked,
+        detail: { checked: this.checked }
+      });
+      this.dispatchEvent(event);
+    },
+
+    _clearNode(node) {
+      while (node.hasChildNodes()) {
+        node.removeChild(node.lastChild);
+      }
+    },
+    _attachEvents() {
+      if (!this._keyboardAttached) {
+        this.$el.addEventListener("keydown", event => {
+          if (event.keyCode === 13 || event.keyCode === 32) {
+            event.preventDefault();
+            this._toggleCheck();
+          }
+        });
+        this._keyboardAttached = true;
+      }
+    }
+  }
+};
+</script>
+
+<style>
+.host {
+  display: block;
+  font-family: inherit;
+  outline: none;
+}
+
+.host.disabled {
+  opacity: 0.6 !important;
+  cursor: default;
+  pointer-events: none;
+}
+
+.host.disabled svg {
+  background: rgba(0, 0, 0, 0.07);
+}
+
+.host.pending {
+  opacity: 0;
+}
+
+:host(:focus) path {
+  stroke-width: 1.5;
+}
+
+#container {
+  display: inline-block;
+  white-space: nowrap;
+}
+
+.inline {
+  display: inline-block;
+  vertical-align: middle;
+}
+
+#checkPanel {
+  cursor: pointer;
+}
+
+svg {
+  display: block;
+}
+
+svg >>> path {
+  stroke: var(--wired-checkbox-icon-color, currentColor);
+  stroke-width: 0.7;
+}
+</style>
---
+++ src/card.vue
@@ -0,0 +1,144 @@
+<template>
+  <div class="host">
+    <div>
+      <slot></slot>
+      <!-- <slot @slotchange="${() => this.requestUpdate()}"></slot> -->
+    </div>
+    <div class="overlay">
+      <svg id="svg"></svg>
+    </div>
+  </div>
+</template>
+
+<script>
+import { wired } from "./wired-lib.js";
+export default {
+  props: {
+    elevation: {
+      type: Number,
+      default: 1
+    }
+  },
+  mounted() {
+    // createRenderRoot
+    this.$el.classList.add("pending");
+
+    // connectedCallback
+    console.log(this.$el.resizeHandler);
+    if (!this.$el.resizeHandler) {
+      this.$el.resizeHandler = this._debounce(
+        this.updated.bind(this.$el),
+        200,
+        false,
+        this.$el
+      );
+      window.addEventListener("resize", this.$el.resizeHandler);
+    }
+    setTimeout(() => this.updated());
+
+    // disconnectedCallback
+    if (this.resizeHandler) {
+        console.log('removeEventListener')
+      window.removeEventListener("resize", this.resizeHandler);
+      delete this.resizeHandler;
+    }
+  },
+  methods: {
+    _clearNode(node) {
+      while (node.hasChildNodes()) {
+        node.removeChild(node.lastChild);
+      }
+    },
+    _debounce(func, wait, immediate, context) {
+      let timeout = 0;
+      return () => {
+        const args = arguments;
+        const later = () => {
+          timeout = 0;
+          if (!immediate) {
+            func.apply(context, args);
+          }
+        };
+        const callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = window.setTimeout(later, wait);
+        if (callNow) {
+          func.apply(context, args);
+        }
+      };
+    },
+    updated() {
+      const svg = this.$el.querySelector("#svg");
+      this._clearNode(svg);
+      var s = this.$el.getBoundingClientRect();
+      var elev = Math.min(Math.max(1, this.elevation), 5);
+      var w = s.width + (elev - 1) * 2;
+      var h = s.height + (elev - 1) * 2;
+      svg.setAttribute("width", w);
+      svg.setAttribute("height", h);
+      wired.rectangle(svg, 0, 0, s.width, s.height);
+      for (var i = 1; i < elev; i++) {
+        wired.line(
+          svg,
+          i * 2,
+          s.height + i * 2,
+          s.width + i * 2,
+          s.height + i * 2
+        ).style.opacity = (85 - i * 10) / 100;
+        wired.line(
+          svg,
+          s.width + i * 2,
+          s.height + i * 2,
+          s.width + i * 2,
+          i * 2
+        ).style.opacity = (85 - i * 10) / 100;
+        wired.line(
+          svg,
+          i * 2,
+          s.height + i * 2,
+          s.width + i * 2,
+          s.height + i * 2
+        ).style.opacity = (85 - i * 10) / 100;
+        wired.line(
+          svg,
+          s.width + i * 2,
+          s.height + i * 2,
+          s.width + i * 2,
+          i * 2
+        ).style.opacity = (85 - i * 10) / 100;
+      }
+      this.$el.classList.remove("pending");
+    }
+  }
+};
+</script>
+
+<style scoped>
+.host {
+  display: inline-block;
+  position: relative;
+  padding: 5px;
+}
+
+.host.pending {
+  opacity: 0;
+}
+
+.overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  pointer-events: none;
+}
+
+svg {
+  display: block;
+}
+svg >>> path {
+  stroke: currentColor;
+  stroke-width: 0.7;
+  fill: transparent;
+}
+</style>
