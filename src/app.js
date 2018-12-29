328eb931c52e32b607abaf944b946cd6185ef1ef
qq919006380 <919006380@qq.com>
2018-12-29 11:24:29 +0800

    初步添加combo组件

--- src/app.js
+++ src/app.js
@@ -4,14 +4,15 @@ import Button from './button';
 import Input from './input';
 import Card from './card';
 import Checkbox from './checkbox';
-
-
-
+import Combo from './combo';
+import Item from './item';
 
 Vue.component('wired-button',Button)
 Vue.component('wired-input',Input)
 Vue.component('wired-card',Card)
 Vue.component('wired-checkbox',Checkbox)
+Vue.component('wired-combo',Combo)
+Vue.component('wired-item',Item)
 
 
 new Vue({
---
+++ src/item.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="host">
+    <span>{{text}}</span>
+  </div>
+</template>
+
+<script>
+import { wired } from "./wired-lib.js";
+export default {
+  props: {
+    text: {
+      type: String
+    },
+    value: {
+      type: String
+    }
+  },
+
+  mounted() {
+    //   connectedCallback
+    this.$el._itemClickHandler = event => {
+      this.$el._onClick(event);
+    };
+    this.$el.addEventListener("click", this.$el._itemClickHandler);
+    // disconnectedCallback
+    if (this._itemClickHandler) {
+      this.$el.removeEventListener("click", this.$el._itemClickHandler);
+      this.$el._itemClickHandler = null;
+    }
+  },
+  methods: {
+    _onClick(e) {
+      const event = new CustomEvent("item-click", {
+        bubbles: true,
+        composed: true,
+        detail: { text: this.text, value: this.value }
+      });
+      this.dispatchEvent(event);
+    }
+  }
+};
+</script>
+
+<style>
+.host {
+  display: block;
+  padding: 8px;
+  font-family: inherit;
+}
+</style>
