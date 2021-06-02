47ea9d1e291bc6b40fb562ec72d7d19f539f2bf8
weibo <919006380@qq.com>
2021-06-02 19:22:59 +0800

    更新打包配置

--- build/webpack.build.conf.js
+++ build/webpack.build.conf.js
@@ -1,19 +1,31 @@
 // 压缩插件，压缩ES6的语法不会报错，uglifyjs会报错(3.0版本支持)
 const TerserWebpackPlugin = require('terser-webpack-plugin');
 const { VueLoaderPlugin } = require('vue-loader');
+const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
     mode: "none",
     entry: {
         'pencil': path.resolve(__dirname, '../packages/index.js'),
-        'pencil.min': path.resolve(__dirname, '../packages/index.js')
+        // 'pencil.min': path.resolve(__dirname, '../packages/index.js')
     },
     output: {
+        path: path.resolve(__dirname, '../dist'),
+        publicPath: '/dist/',
         filename: '[name].js',
         library: 'pencil', // 指定库的名称，及库的全局变量
         libraryTarget: 'umd', // 支持库引入的方式
+        umdNamedDefine: true,    // 是否将模块名称作为 AMD 输出的命名空间
         libraryExport: 'default'
+    },
+    externals: {
+        vue: {
+            root: 'Vue',
+            commonjs: 'vue',
+            commonjs2: 'vue',
+            amd: 'vue'
+        }
     },
     module: { // 用来配置第三方loader模块的  
         rules: [ // 文件的匹配规则 
@@ -24,7 +36,11 @@ module.exports = {
         ]
     },
     plugins: [
-        new VueLoaderPlugin()
+        new VueLoaderPlugin(),
+        new webpack.DefinePlugin({
+            __VUE_OPTIONS_API__: JSON.stringify(true),
+            __VUE_PROD_DEVTOOLS__: JSON.stringify(false),
+        }),
     ],
     optimization: {
         minimize: true,
